97c9e2ae923b4465f88dfd036c190831b33fd79b
grantfogle <grantjfogle@gmail.com>
2018-11-27 09:36:49 -0700

    set up app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const listener = () => console.log(`Listening on port ${port}!`);
+app.listen(port, listener);
+module.exports = app
+
