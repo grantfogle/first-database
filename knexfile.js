bfa956cfbba16049b1d02d06b310b011673bfa82
grantfogle <grantjfogle@gmail.com>
2018-11-27 10:19:22 -0700

    setting up knex files

--- knexfile.js
+++ knexfile.js
@@ -6,26 +6,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgresql://localhost/FIRST_DATABASE'
   },
-
-  staging: {
-    // client: 'postgresql',
-    // connection: {
-    //   database: 'my_db',
-    //   user:     'username',
-    //   password: 'password'
-    // },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   tableName: 'knex_migrations'
-    // }
-  },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   }
-
 };
---
+++ migrations/20181127095613_students.js
@@ -0,0 +1,10 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('table_name', (entityInTable) => {
+        entityInTable.increments('id')
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('table_name')
+};
