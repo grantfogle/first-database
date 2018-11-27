7d6475cb5131571601ea23b2f12980b4f866945e
grantfogle <grantjfogle@gmail.com>
2018-11-27 09:37:32 -0700

    set up first route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('The Route Worked!')
+})
+
 const listener = () => console.log(`Listening on port ${port}!`);
 app.listen(port, listener);
 module.exports = app
