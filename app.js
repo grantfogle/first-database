2bb9c11cf774d192416b250babd6fdf3d9d767c5
grantfogle <grantjfogle@gmail.com>
2018-11-27 10:40:34 -0700

    figuring out how to view databse in the cli

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const knex = require('knex')
 
 app.get('/', (req, res) => {
     res.send('The Route Worked!')
 })
+
+console.log(knex)
 
 const listener = () => console.log(`Listening on port ${port}!`);
 app.listen(port, listener);
--- migrations/20181127095613_students.js
+++ migrations/20181127095613_students.js
@@ -1,10 +1,16 @@
-
+//makes table
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable('table_name', (entityInTable) => {
-        entityInTable.increments('id')
+    return knex.schema.createTable('students', (student) => {
+        student.increments('id')
+        student.string('name')
+        student.string('fave_animal')
+        student.string('previous_occupation')
+        student.integer('hometown_lat')
+        student.integer('hometown_long')
+        student.string('useless_superpower')
     })
 };
-
+//drops it/ remakes table
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists('table_name')
+    return knex.schema.dropTableIfExists('students')
 };
