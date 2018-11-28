4b8f6f939e7293f10d225ab357f769b51a79762c
grantfogle <grantjfogle@gmail.com>
2018-11-28 10:20:12 -0700

    added getById and post requests

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 const knex = require('knex')
 const queries = require('./queries')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser())
+app.use(cors())
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    queries.getById(id).then(students => res.send(students))
+})
 
 app.get('/', (req, res) => {
     queries.listAll().then(students => res.send(students))
+})
+
+app.post('/', (req, res) => {
+    queries.createStudent(req.body).then(students => res.send(students[0]))
 })
 
 console.log(knex)
 
 const listener = () => console.log(`Listening on port ${port}!`);
 app.listen(port, listener);
-module.exports = app
+// module.exports = app
 
--- queries.js
+++ queries.js
@@ -3,5 +3,12 @@ const database = require('./database/connection')
 module.exports = {
     listAll() {
         return database('students')
+    },
+    getById(id) {
+        return database('students').where('id', id)
+    },
+    createStudent(newStudent) {
+        return database('students').insert(newStudent).returning('*')
     }
+
 }
